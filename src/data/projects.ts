bdf70683db6f38b1abf2e0704afd6e025e9fc934
MiguelAChavez <chavez.miguel.angel154@gmail.com>
2025-11-19 17:18:06 -0300

    Add core components for portfolio site: Header, Footer, Hero, ProjectCard, and UI elements

---
+++ src/data/projects.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Project One",
+    description: "A brief description of Project One.",
+    link: "https://example.com/project-one",
+    github: "https://github.com/username/project-one",
+    image: "/assets/img/project-one.png",
+  },
+  {
+    id: 2,
+    title: "Project Two",
+    description: "A brief description of Project Two.",
+    link: "https://example.com/project-two",
+    github: "https://github.com/username/project-two",
+    image: "/assets/img/project-two.png",
+  },
+  {
+    id: 3,
+    title: "Project Three",
+    description: "A brief description of Project Three.",
+    link: "https://example.com/project-three",
+    github: "https://github.com/username/project-three",
+    image: "/assets/img/project-three.png",
+  },
+  {
+    id: 4,
+    title: "Project Four",
+    description: "A brief description of Project Four.",
+    link: "https://example.com/project-four",
+    github: "https://github.com/username/project-four",
+    image: "/assets/img/project-four.png",
+  },
+];
